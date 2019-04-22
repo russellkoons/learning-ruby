507ed88c9af49b1b32fdce679fdd613d4861d704
Russell Koons <rkoons@gmail.com>
2019-04-22 14:06:43 -0400

    comments

---
+++ comments.rb
@@ -0,0 +1,11 @@
+# This is a comment
+
+puts "Hello World" # prints "Hello World" to the console
+
+# In this section of code
+# we are going to do operations
+# A, B, C
+
+puts 1 + 1
+puts 5 + 3
+p "blah blah blah"
