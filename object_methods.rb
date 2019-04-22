c2308b70dc8781f3eb8ad2ad1dc2c9ffb00a8c51
Russell Koons <rkoons@gmail.com>
2019-04-22 16:03:20 -0400

    object methods

---
+++ object_methods.rb
@@ -0,0 +1,20 @@
+p "Hello world".length
+
+p "Hello world".upcase
+
+p "HELLO WORLD".downcase
+
+foo = "hello world"
+p foo.length
+p foo.upcase
+
+puts
+
+p 10.next
+p -1.next
+
+puts
+
+puts "Hello world"
+puts "Hello world".inspect
+p "Hello world"
