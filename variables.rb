232c4f84c22c41cb158050eaedb5621dbbf3fb69
Russell Koons <rkoons@gmail.com>
2019-04-22 15:42:13 -0400

    Variables

---
+++ variables.rb
@@ -0,0 +1,23 @@
+name = "Russell"
+last_name = "Koons"
+handsome = false
+age = 17 + 13
+
+puts name
+puts last_name
+
+puts name + " " + last_name
+
+puts handsome
+
+puts age
+
+age = 50
+
+puts age
+
+fave_number = 17
+p fave_number
+
+fave_number = "Seventeen"
+p fave_number
