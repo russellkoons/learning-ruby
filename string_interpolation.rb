24365baa7f6dd54425602ccc65043b74c0e7578c
Russell Koons <rkoons@gmail.com>
2019-04-22 16:23:40 -0400

    string interpolation

---
+++ string_interpolation.rb
@@ -0,0 +1,12 @@
+name = "Russell"
+
+p "Hello #{name}, how are you?"
+
+age = 30
+
+p "I am " + age.to_s + " years old" # Hard
+p "I am #{age} years old" # Easy
+
+p "The result of adding 1 + 1 is #{1 + 1}"
+
+p "In five years I will be #{age + 5}"
