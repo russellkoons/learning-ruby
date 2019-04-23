9daaaf6a6c006de5b80a4fef11ec86285f133965
Russell Koons <rkoons@gmail.com>
2019-04-23 10:59:58 -0400

    float methods

---
+++ module_2/float_methods.rb
@@ -0,0 +1,8 @@
+p 10.5.floor # rounds down
+p 10.5.ceil # rounds up
+
+p 3.14159.round #Rounds up or down if > or < .5
+p 3.14159.round(3) # rounds to 3 decimal places
+
+p 35.67.abs #Gets distance from 0
+p -35.67.abs
