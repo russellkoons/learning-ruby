43a714168e14ec472181e54e0e63503373b04f47
Russell Koons <rkoons@gmail.com>
2019-04-23 10:29:57 -0400

    comparison methods

---
+++ module_2/comparison.rb
@@ -0,0 +1,16 @@
+# in ruby == is equality and != is inequality
+
+# Less than <
+p 1 < 8
+
+p 12 < 5
+
+#Less than or equal to <=
+p 1 <= 1
+p 1 <= 3
+
+# greater than > or equal to >=
+p 10 > 5
+p 5 > 10
+
+# Works on floats too
