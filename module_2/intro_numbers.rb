21f79ee43d3bce60b5d31b8ec67b63bb18c63154
Russell Koons <rkoons@gmail.com>
2019-04-23 10:11:01 -0400

    numbers intro

---
+++ module_2/intro_numbers.rb
@@ -0,0 +1,12 @@
+p 5.class
+p 0.class
+p 100.class
+p -837.class
+
+p 5.987.class
+p 2.71.class
+p -10.23.class
+
+p 999999999999999999999999999999999999999999999999999999.class
+
+# Fixnum and bignum are deprecated and are now called integer
