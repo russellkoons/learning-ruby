c8c466ebecc54edb2c6a78521c75a0b408aa0fe2
Russell Koons <rkoons@gmail.com>
2019-04-26 10:19:47 -0400

    elsif

---
+++ module_3/07elsif.rb
@@ -0,0 +1,19 @@
+color = "black"
+
+if color == "red"
+  puts "Red is cool"
+elsif color == "green"
+  puts "Green is ok"
+elsif color == "yellow"
+  puts "yellow smellows"
+end
+
+number = 55
+
+if number < 25
+  puts "That's a small number"
+elsif number < 50
+  puts "That's a middle number"
+elsif number < 75
+  puts "That's a big number"
+end  
