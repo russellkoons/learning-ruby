13ece5e49197a49593dac4de6f22e6a36344bbca
Russell Koons <rkoons@gmail.com>
2019-04-26 10:00:42 -0400

    if statements

---
+++ module_3/05ifstatement.rb
@@ -0,0 +1,19 @@
+if 5 < 2
+  puts "That math statement is true"
+end
+
+password = "buppert"
+
+if password == "topsecret"
+  puts "Congrats, your password sucks"
+end
+
+word = "kangaroo"
+
+if word.length == 8
+  puts "That word has 8 letters"
+end
+
+if word.include?("ga")
+  puts "Yep, it has ga"
+end
