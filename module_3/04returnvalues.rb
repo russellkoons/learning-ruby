fe4cce90d30f0d770199e5aafd0b58abd31d1274
Russell Koons <rkoons@gmail.com>
2019-04-26 09:50:27 -0400

    return values

---
+++ module_3/04returnvalues.rb
@@ -0,0 +1,19 @@
+def add_two(num1, num2)
+  puts "Ok I'm solving your problem"
+  return num1 + num2
+end
+
+p add_two(163, 420)
+p add_two(17, 19)
+
+def return_string
+  "What will be the return value here?"
+end
+
+def return_guess
+  puts "What will be the return value here?"
+  print "Will print be different?"
+end
+
+p return_string
+p return_guess
