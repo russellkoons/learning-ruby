a57c8c1b8232d1314c0fe5ce8bb2a3930973cef9
russellkoons <rkoons@gmail.com>
2019-08-26 17:25:44 -0400

    Case statements

--- module_4/01case_statement.rb
+++ module_4/01case_statement.rb
@@ -1,3 +1,5 @@
+# Case statements are an easier way to do long ifels statements
+
 def rate_my_food(food)
   case food
   when "Steak"
@@ -16,3 +18,20 @@ end
 puts rate_my_food("Sushi")
 puts rate_my_food("Tofu")
 puts rate_my_food("Pizza")
+
+# Cut down on line space with then
+
+def calculate_grade(grade)
+  case grade
+  when 90..100 then "A"
+  when 80..89 then "B"
+  when 70..79 then "C"
+  when 60..69 then "D"
+  else "F"
+  end
+end
+
+puts calculate_grade(99)
+puts calculate_grade(69)
+puts calculate_grade(75)
+puts calculate_grade(42)
