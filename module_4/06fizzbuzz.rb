3230c7e44f89b9fc6e69a0d561b46a2fb41fbafb
russellkoons <rkoons@gmail.com>
2019-08-27 17:13:58 -0400

    fizzbuzz

---
+++ module_4/06fizzbuzz.rb
@@ -0,0 +1,19 @@
+# Fizzbuzz!
+
+def fizzbuzz(num)
+  i = 1
+  until i > num
+    if i % 15 == 0
+      puts "fizzbuzz"
+    elsif i % 3 == 0
+      puts "fizz"
+    elsif i % 5 == 0
+      puts "buzz"
+    else
+      puts i
+    end
+    i += 1
+  end
+end
+
+fizzbuzz(69)
