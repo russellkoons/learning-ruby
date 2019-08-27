a6399c192213b6f1b4a8cab76b74a530c9f50559
russellkoons <rkoons@gmail.com>
2019-08-27 17:05:03 -0400

    until

---
+++ module_4/05until.rb
@@ -0,0 +1,16 @@
+# Until Keyword
+# Works like a while loop but loops UNTIL the condition is true
+
+i = 0
+
+# These two loops have the same output
+
+# while i < 10
+#   puts i
+#   i += 1
+# end
+
+until i > 9
+  puts i
+  i += 1
+end
