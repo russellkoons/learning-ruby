e31b7a98d48ea3c0d48a6501b803fb5e7c694168
russellkoons <rkoons@gmail.com>
2019-08-30 15:00:04 -0400

    alphabetical ranges

---
+++ module_5/02alphabetical_ranges.rb
@@ -0,0 +1,7 @@
+# Alphabetical Ranges
+# Similar but with letters instead
+
+alphabet = "A".."z" # Range of all characters of the alphabet in upper and lower case, plus extra
+# characters [\]^_`
+
+puts alphabet.first(40)
