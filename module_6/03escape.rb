60ebe4f1c6f6d2850c072ab0b8d73de61d373975
russellkoons <rkoons@gmail.com>
2019-09-02 15:11:38 -0400

    escape characters

---
+++ module_6/03escape.rb
@@ -0,0 +1,9 @@
+# Escape characters
+
+puts "Hello"
+puts 'Hello'
+
+puts 'Juliet said "Goodbye" to Romeo'
+puts "Juliet said 'Goodbye' to Romeo"
+
+puts "Juliet said \"Goodbye\" to Romeo" # Gotta use a backslash to escape the characters
